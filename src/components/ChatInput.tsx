4cd60bc0a7c189300462e0b50f8de6373435dbf1
vishnukumarkvs <kvsvishnukumar@gmail.com>
2023-05-07 00:28:43 +0530

    added loading button

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -8,6 +8,7 @@ import { nanoid } from "nanoid";
 import { Message } from "@/lib/validators/message";
 import { MessagesContext } from "@/context/messages";
 import { set } from "zod";
+import { CornerDownLeft, Loader2 } from "lucide-react";
 
 interface ChatInputProps extends HTMLAttributes<HTMLDivElement> {}
 
@@ -87,11 +88,26 @@ const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
               sendMessage(message);
             }
           }}
+          disabled={isLoading}
           value={input}
           onChange={(e) => setInput(e.target.value)}
           autoFocus
           placeholder="Write a message..."
           className="peer disabled:opacity-50 pr-14 resize-none block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6"
+        />
+        <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
+          <kbd className="inline-flex items-center rounded border bg-white border-gray-200 px-1 font-sans text-xs text-gray-400">
+            {isLoading ? (
+              <Loader2 className="w-3 h-3 animate-spin" />
+            ) : (
+              <CornerDownLeft className="w-3 h-3" />
+            )}
+          </kbd>
+        </div>
+
+        <div
+          className="absolute inset-x-0 bottom-0 border-t border-gray-300 peer-focus:border-t-2 peer-focus:border-indigo-600"
+          aria-hidden="true"
         />
       </div>
     </div>
